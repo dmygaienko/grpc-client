93cbcc8bef0e166f6c49fff250acfe07b12dc664
dmytro.mygaienko <dmygaenko@gmail.com>
2019-05-01 19:27:28 +0300

    add file transfer

--- src/main/java/com/dmygaenko/grpcclient/HelloApi.java
+++ src/main/java/com/dmygaenko/grpcclient/HelloApi.java
@@ -1,6 +1,5 @@
 package com.dmygaenko.grpcclient;
 
-import com.dmygaenko.grpcapi.HelloResponse;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -20,5 +19,11 @@ public class HelloApi {
     public String hello(@RequestParam String firstName, @RequestParam String lastName) {
         log.info("Hello Api is called with firstName - {}, lastName - {}", firstName, lastName);
         return helloClient.hello(firstName, lastName);
+    }
+
+    @GetMapping(path = "/transfer")
+    public String transfer(@RequestParam String firstName, @RequestParam String lastName) {
+        log.info("Transfer Api is called with firstName - {}, lastName - {}", firstName, lastName);
+        return helloClient.transfer(firstName, lastName);
     }
 }
--- src/main/java/com/dmygaenko/grpcclient/HelloClient.java
+++ src/main/java/com/dmygaenko/grpcclient/HelloClient.java
@@ -1,24 +1,40 @@
 package com.dmygaenko.grpcclient;
 
-import com.dmygaenko.grpcapi.HelloRequest;
-import com.dmygaenko.grpcapi.HelloResponse;
-import com.dmygaenko.grpcapi.HelloServiceGrpc;
+import com.dmygaenko.api.*;
+import com.google.protobuf.ByteString;
 import io.grpc.ManagedChannel;
 import io.grpc.ManagedChannelBuilder;
 import org.springframework.stereotype.Service;
+
+import java.nio.charset.Charset;
 
 @Service
 public class HelloClient {
 
     public String hello(String firstName, String lastName) {
-        final ManagedChannel channel = ManagedChannelBuilder.forAddress("localhost", 6565)
-                .usePlaintext()
-                .build();
-        final HelloServiceGrpc.HelloServiceBlockingStub stub = HelloServiceGrpc.newBlockingStub(channel);
+        final HelloServiceGrpc.HelloServiceBlockingStub stub = getHelloServiceBlockingStub();
         HelloResponse hello = stub.hello(
                 HelloRequest.newBuilder()
                         .setFirstName(firstName)
                         .setLastName(lastName)
+                        .build());
+        return hello.getGreeting();
+    }
+
+    private HelloServiceGrpc.HelloServiceBlockingStub getHelloServiceBlockingStub() {
+        final ManagedChannel channel = ManagedChannelBuilder.forAddress("localhost", 6565)
+                .usePlaintext()
+                .build();
+        return HelloServiceGrpc.newBlockingStub(channel);
+    }
+
+    public String transfer(String firstName, String lastName) {
+        final HelloServiceGrpc.HelloServiceBlockingStub stub = getHelloServiceBlockingStub();
+        FileResponse hello = stub.transferFile(
+                FileRequest.newBuilder()
+                        .setFirstName(firstName)
+                        .setLastName(lastName)
+                        .setContent(ByteString.copyFrom("Test String", Charset.defaultCharset()))
                         .build());
         return hello.getGreeting();
     }
